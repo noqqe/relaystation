d8943132bb92d4195208cafb926ddb517e40943e
Florian Baumann <flo@noqqe.de>
2022-11-18 10:02:59 +0100

    Streaming from twitter works

--- src/relaystation/root.go
+++ src/relaystation/root.go
@@ -1,14 +1,9 @@
 package relaystation
 
 import (
-	"context"
-	"fmt"
 	"log"
-	"os"
 
 	"github.com/michimani/gotwi"
-	"github.com/michimani/gotwi/tweet/filteredstream"
-	"github.com/michimani/gotwi/tweet/filteredstream/types"
 )
 
 func newOAuth2Client() (*gotwi.Client, error) {
@@ -22,117 +17,31 @@ func newOAuth2Client() (*gotwi.Client, error) {
 
 func Root() {
 
-	r := loadRules()
-	// log.Println(r)
-	os.Exit(1)
-	log.Println("Cleaning rules")
-	_, to_clear := listSearchStreamRules()
+	var to_create Rules
+	var to_delete Rules
 
-	deleteSearchStreamRules(to_clear)
-	//listSearchStreamRules
-	createSearchStreamRules(os.Getenv("RULE_1"))
-	//listSearchStreamRules
-	execSearchStream()
+	log.Println("Loading rules from environment")
+	to_create = loadRules()
 
-}
+	log.Println("Fetching current rules to delete")
+	_, to_delete = listSearchStreamRules()
 
-func execSearchStream() {
-	c, err := newOAuth2Client()
-	if err != nil {
-		fmt.Println(err)
-		return
+	log.Println("Clearing current rules")
+	for _, rule := range to_delete {
+		deleteSearchStreamRules(rule)
 	}
 
-	p := &types.SearchStreamInput{}
-
-	s, err := filteredstream.SearchStream(context.Background(), c, p)
-	if err != nil {
-		fmt.Println(err)
-		return
+	log.Println("Create new rules")
+	for _, rule := range to_create {
+		createSearchStreamRules(rule)
 	}
 
-	for s.Receive() {
-		t, err := s.Read()
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			if t != nil {
-				fmt.Println(gotwi.StringValue(t.Data.ID), gotwi.StringValue(t.Data.Text))
-			}
-		}
-	}
-}
+	log.Println("Current rules configuration:")
+	listSearchStreamRules()
 
-func createSearchStreamRules(keyword string) {
-	c, err := newOAuth2Client()
-	if err != nil {
-		fmt.Println(err)
-		return
+	log.Println("Starting stream...")
+	for {
+		execSearchStream()
 	}
 
-	p := &types.CreateRulesInput{
-		Add: []types.AddingRule{
-			{Value: gotwi.String(keyword), Tag: gotwi.String(keyword)},
-		},
-	}
-
-	res, err := filteredstream.CreateRules(context.TODO(), c, p)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-	for _, r := range res.Data {
-		fmt.Printf("ID: %s, Value: %s, Tag: %s\n", gotwi.StringValue(r.ID), gotwi.StringValue(r.Value), gotwi.StringValue(r.Tag))
-	}
-}
-
-// createSearchStreamRules lists search stream rules.
-func listSearchStreamRules() (error, Rules) {
-
-	rules := make(Rules, 5)
-	c, err := newOAuth2Client()
-	if err != nil {
-		fmt.Println(err)
-		return fmt.Errorf("Could not get OAuth2Client. Check credentials"), rules
-	}
-
-	p := &types.ListRulesInput{}
-	res, err := filteredstream.ListRules(context.Background(), c, p)
-	if err != nil {
-		fmt.Println(err.Error())
-		return fmt.Errorf("Could not get rules"), rules
-	}
-
-	for i, r := range res.Data {
-		rules[i] = gotwi.StringValue(r.ID)
-		fmt.Printf("ID: %s, Value: %s, Tag: %s\n", gotwi.StringValue(r.ID), gotwi.StringValue(r.Value), gotwi.StringValue(r.Tag))
-	}
-	return nil, rules
-}
-
-func deleteSearchStreamRules(ruleID string) {
-	c, err := newOAuth2Client()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	p := &types.DeleteRulesInput{
-		Delete: &types.DeletingRules{
-			IDs: []string{
-				ruleID,
-			},
-		},
-	}
-
-	res, err := filteredstream.DeleteRules(context.TODO(), c, p)
-	if err != nil {
-		fmt.Println(err.Error())
-		return
-	}
-
-	for _, r := range res.Data {
-		fmt.Printf("ID: %s, Value: %s, Tag: %s\n", gotwi.StringValue(r.ID), gotwi.StringValue(r.Value), gotwi.StringValue(r.Tag))
-	}
 }
---
+++ src/relaystation/twitterstream.go
@@ -0,0 +1,118 @@
+package relaystation
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/michimani/gotwi"
+	"github.com/michimani/gotwi/tweet/filteredstream"
+	"github.com/michimani/gotwi/tweet/filteredstream/types"
+)
+
+func execSearchStream() {
+	c, err := newOAuth2Client()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	p := &types.SearchStreamInput{}
+
+	s, err := filteredstream.SearchStream(context.Background(), c, p)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	for s.Receive() {
+		t, err := s.Read()
+		if err != nil {
+			log.Println(err)
+		} else {
+			if t != nil {
+				fmt.Println(gotwi.StringValue(t.Data.AuthorID), gotwi.StringValue(t.Data.ID), gotwi.StringValue(t.Data.Text))
+			}
+		}
+	}
+}
+
+func createSearchStreamRules(keyword string) {
+	c, err := newOAuth2Client()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	p := &types.CreateRulesInput{
+		Add: []types.AddingRule{
+			{Value: gotwi.String(keyword), Tag: gotwi.String(keyword)},
+		},
+	}
+
+	res, err := filteredstream.CreateRules(context.TODO(), c, p)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	for _, r := range res.Data {
+		log.Printf("Rule: %s\n", gotwi.StringValue(r.Value))
+	}
+}
+
+func listSearchStreamRules() (error, Rules) {
+
+	rules := make(Rules, 5)
+
+	c, err := newOAuth2Client()
+	if err != nil {
+		log.Println(err)
+		return fmt.Errorf("Could not get OAuth2Client. Check credentials"), rules
+	}
+
+	p := &types.ListRulesInput{}
+	res, err := filteredstream.ListRules(context.Background(), c, p)
+	if err != nil {
+		log.Println(err.Error())
+		return fmt.Errorf("Could not get rules"), rules
+	}
+
+	for i, r := range res.Data {
+		rules[i] = gotwi.StringValue(r.ID)
+		log.Printf("ID: %s, Value: %s\n", gotwi.StringValue(r.ID), gotwi.StringValue(r.Value))
+	}
+
+	return nil, rules
+}
+
+func deleteSearchStreamRules(ruleID string) {
+	c, err := newOAuth2Client()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	if ruleID == "" {
+		return
+	}
+
+	log.Println("Deleting", ruleID)
+	p := &types.DeleteRulesInput{
+		Delete: &types.DeletingRules{
+			IDs: []string{
+				ruleID,
+			},
+		},
+	}
+
+	res, err := filteredstream.DeleteRules(context.TODO(), c, p)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	for _, r := range res.Data {
+		log.Println(gotwi.StringValue(r.Value))
+	}
+}
